b95520a8b8465999d8078081af30fd68fe8cb786
robbie <Robbie.Robinette@gmail.com>
2025-10-07 18:07:29 -0500

    rename twin_test.py

--- twin_test.sh
+++ twin_test.sh
@@ -3,6 +3,6 @@
 
 mkdir -p out-tt
 for i in primary condorcet irv actual; do
-	examine_toxicity.py --candidates 2 --election-type $i > out-tt/$i.out 2>&1 &
+	twin_test.py --candidates 2 --election-type $i > out-tt/$i.out 2>&1 &
 done
 wait
